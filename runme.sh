36813a4538c0ac81424cedec65023b9aa5c408e6
Christopher Ferris <chrisfer@us.ibm.com>
2016-07-21 09:54:43 -0400

    initial commit
    initial commit
    
    Signed-off-by: Christopher Ferris <chrisfer@us.ibm.com>

---
+++ runme.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -f rest_api.json ]; then
+    echo "Downloading rest_api.json from GitHub hyperledger/fabric repository"
+    curl -XGET https://raw.githubusercontent.com/hyperledger/fabric/master/core/rest/rest_api.json --output rest_api.json
+    if [ $? -eq 0 ]; then
+      echo OK
+    else
+      echo "Unable to download rest_api.json"
+      exit 1
+    fi
+fi
+which npm
+if [ $? -eq 1 ]; then
+  npm install http-server
+  if [ $? -eq 1 ]; then
+    echo "Unable to install http-server npm module"
+    exit 1
+  fi
+fi
+
+if [ ! -f swagger-ui/dist/index.html ]; then
+  git clone https://github.com/swagger-api/swagger-ui.git
+  if [ $? -eq 1 ]; then
+    echo "Unable to clone swagger-ui"
+    exit 1
+  fi
+  cp -r swagger-ui/dist/* .
+  rm index.html
+  cp fabric.html index.html
+fi
+
+rm http-server.log
+http-server -a 0.0.0.0 -p 5554 -o --cors &> http-server.log &
+exit $?
